be2d012cff3d6487b52c8cb0e31c2b181ff194e0
N0vaPixel <aurelien.hernandez27@gmail.com>
2017-05-01 20:18:39 +0200

    First commit

---
+++ esp8266_embedded_mq135_webserver/MQ135.h
@@ -0,0 +1,58 @@
+/**************************************************************************/
+/*!
+@file     MQ135.h
+@author   G.Krocker (Mad Frog Labs)
+@license  GNU GPLv3
+
+First version of an Arduino Library for the MQ135 gas sensor
+TODO: Review the correction factor calculation. This currently relies on
+the datasheet but the information there seems to be wrong.
+
+@section  HISTORY
+
+v1.0 - First release
+*/
+/**************************************************************************/
+#ifndef MQ135_H
+#define MQ135_H
+#if ARDUINO >= 100  // Détecte la version de l'IDE Arduino
+ #include "Arduino.h"
+#else
+ #include "WProgram.h"
+#endif
+
+/// The load resistance on the board + à vérifier dans la doc constructeur.
+#define RLOAD 10.0
+/// Calibration resistance at atmospheric CO2 level
+#define RZERO 76.63
+/// Parameters for calculating ppm of CO2 from sensor resistance
+// Ce sont les paramètres de l'équation : A * x^B
+#define PARA 116.6020682
+#define PARB 2.769034857
+
+/// Parameters to model temperature and humidity dependence
+// C'est inutile pour le moment car il faut des capteurs de température et de pression.
+#define CORA 0.00035
+#define CORB 0.02718
+#define CORC 1.39538
+#define CORD 0.0018
+
+/// Atmospheric CO2 level for calibration purposes
+// En 2017 autour de 400 ppm
+#define ATMOCO2 397.13
+
+class MQ135 {
+ private:
+  uint8_t _pin;
+
+ public:
+  MQ135(uint8_t pin);
+  float getCorrectionFactor(float t, float h);
+  float getResistance();
+  float getCorrectedResistance(float t, float h);
+  float getPPM();
+  float getCorrectedPPM(float t, float h);
+  float getRZero();
+  float getCorrectedRZero(float t, float h);
+};
+#endif
